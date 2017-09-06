9433c608e5f895b6f62b656ff963eae8bd3e9900
Kris <kris.weber.1994@gmail.com>
2017-09-06 09:50:42 -0700

    add mongoose, remove remaining Goodreads code

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var expressValidator = require('express-validator');
 
 var index = require('./routes/index');
 var books = require('./routes/books');
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressValidator());
 
 app.use('/', index);
 app.use('/books', books);
