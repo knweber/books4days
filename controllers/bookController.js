8ae5d850314387250a2e8281678d2099b61b6caf
Kris <kris.weber.1994@gmail.com>
2017-09-06 10:15:00 -0700

    Add search method to find a book by title

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,17 +2,26 @@ var Book = require('../models/book');
 var app = require('../app');
 var expressValidator = require('express-validator');
 
-exports.book_search_form = function(req, res, next) {
+exports.bookinstance_search_get = function(req, res, next) {
 
-  req.checkBody('title', 'Title required').notEmpty();
-  req.sanitize('title').escape();
-  req.sanitize('title').trim();
-
-  var errors = req.validationErrors();
-  
   Book.find({}, 'title')
   .exec(function(err, books) {
     if(err) { return next(err); }
     res.render('bookinstance_form', {title: 'Search', book_list: books});
   });
 };
+
+exports.book_search_post = function(req, res, next) {
+  req.checkBody('title', 'Title must be specified.').notEmpty();
+  req.sanitize('title').escape();
+  req.sanitize('title').trim();
+
+  var bookInstance = new BookInstance({
+    title: req.body.book
+  });
+
+  bookInstance.save(function(err) {
+    if(err) { return next(err); }
+    res.redirect(bookinstance.url);
+  })
+}
